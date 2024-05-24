44cf802f27f97b4348de5f3282971b8f8c19ee67
deepblue6 <joerosyleona@gmail.com>
2024-05-24 13:42:13 -0700

    cleaned up

--- futras-master/timer.js
+++ futras-master/timer.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let timer;
   let time = 0;
   const display = document.getElementById("timerDisplay");
+  const timeInput = document.getElementById("timeInput");
+  const setButton = document.getElementById("setButton");
   const startButton = document.getElementById("startButton");
   const pauseButton = document.getElementById("pauseButton");
   const resetButton = document.getElementById("resetButton");
@@ -20,10 +22,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function startTimer() {
-    if (!timer) {
+    if (time > 0 && !timer) {
       timer = setInterval(() => {
-        time++;
-        updateDisplay();
+        if (time > 0) {
+          time--;
+          updateDisplay();
+        } else {
+          clearInterval(timer);
+          timer = null;
+        }
       }, 1000);
     }
   }
@@ -40,6 +47,15 @@ document.addEventListener("DOMContentLoaded", () => {
     updateDisplay();
   }
 
+  function setTimer() {
+    const inputTime = parseInt(timeInput.value, 10);
+    if (!isNaN(inputTime) && inputTime > 0) {
+      time = inputTime;
+      updateDisplay();
+    }
+  }
+
+  setButton.addEventListener("click", setTimer);
   startButton.addEventListener("click", startTimer);
   pauseButton.addEventListener("click", pauseTimer);
   resetButton.addEventListener("click", resetTimer);
